7baa510e48bafd504da04b557607f3979e24a49b
Jonas Sourlier <jonas.sourlier@yolp.ch>
2022-03-18 15:55:50 +0100

    add WebView 2 demo project

---
+++ WebView2Test/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebView2Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.webView21.Source = new Uri("https://en.wikipedia.org/wiki");
+        }
+    }
+}
